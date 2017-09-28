4f87e31d3b4aa97a342f8cd001b5ed3fe4c4a4ea
Seshadrinathan Kothandaraman <kothand@arcesium.com>
2017-09-28 13:12:07 +0530

    initial commit

---
+++ python/dd-openFilesPerUser.py
@@ -0,0 +1,101 @@
+#!/opt/datadog-agent/embedded/bin/python
+# This script will use the datadog agent to upload a report of open files per user on this host. 
+# Author: Seshadri Kothandaraman 28 Sep 2017
+''' 
+Rough approach
+1. get the system-wide limits of processes
+2. get list of users
+3. get the list of processes (PIDs) associated with each user
+4. get the number of filedescriptors associated with each process using /proc/<pid>/fd 
+   (avoid lsof -p <pid> |wc -l as it gives unneeded info)
+   The hard and soft limits applying to the process can be found in /proc/<pid>/limits
+5. build a summary of the total number of filedescriptors for each user
+6. set alert levels on a system-wide and per-user basis
+7. create metrics for Datadog
+8. send metrics to Datadog
+
+The script has to be run as root as /proc/<PID>/fd is not world-readable. If run as a normal user, it will report the metrics 
+only for that user
+
+The other approach would have been to upload the raw count of open processes per user and let Datadog
+handle the alerting. However this approach won't be portable across hosts as individual systems may have 
+different limits.
+
+'''
+
+import psutil 
+from subprocess import Popen, PIPE, STDOUT
+import subprocess
+import resource
+from re import split
+from sys import stdout
+import glob 
+import os
+import logging
+import time
+from datadog import statsd
+# from arcesium.metrics import metricd
+
+# Check if the program is running as root. If not, warn and exit
+
+if os.geteuid != 0:
+	print 'You can run the script as a normal user, but you won\'t get reports of other users. In addition, your own stats might be wrong'
+
+# Generate the list of unique users logged in to this system
+
+print 'Here are users logged in to this host: \n'
+getUsersCmd = "who |awk '{print $1}' |sort -u"
+# print subprocess.check_output(getUsersCmd, shell=True)
+userList = subprocess.check_output(getUsersCmd, shell=True)
+print userList
+
+# Get the max number of open file descriptors allowed on this host
+
+
+with open('/proc/sys/fs/file-max') as f:
+	maxOpenFDs = f.read
+	print 'The max number of open file descriptors allowed on this host is: {}'.format(maxOpenFDs())
+
+# Get limits of file descriptors and threads per user
+# Upon encountering this limit, fork(2) fails with the error EAGAIN.
+
+totalSystemThreads = resource.getrlimit(resource.RLIMIT_NPROC)[0]
+
+print 'The max number of open file descriptors for the current process is {}\n'.format(resource.getrlimit(resource.RLIMIT_NOFILE))
+print 'The max number of threads per user is: {}\n'.format(resource.getrlimit(resource.RLIMIT_NPROC))
+
+
+def postMetric(user):
+	# user = 'kothand'
+	# print 'Getting the PIDS of user: {}'.format(self.user)  
+	print 'Getting the PIDS of user: {}'.format(user)  
+	# psCmd = "ps --no-header -U " + self.user + " -u " + self.user + " u |awk '{print $2}'"
+	psCmd = "ps --no-header -U " + user + " -u " + user + " u |awk '{print $2}'"
+	listOfPIDs = subprocess.check_output(psCmd, shell=True).split('\n')
+	print 'Here are the PIDs: ' 
+	print listOfPIDs
+	# statsd.gauge
+
+
+	totalThreadsByUser = 0
+
+
+	for i in listOfPIDs:
+
+		if len(i) > 0:
+			# print 'The current PID is {}'.format(i)
+			# print files1
+			# print len(glob.glob('/proc/' + i + '/fd/*'))
+			totalThreadsByUser += len(glob.glob('/proc/' + i + '/fd/*'))  # glob is better than system/os wc -l
+
+	percentThreadsByUser = ( totalThreadsByUser * 100 ) / totalSystemThreads
+	print 'The total number of threads used by user "{}" is: {}'.format(user, totalThreadsByUser)
+	print 'The percent of threads used by user {} is:  {}'.format(user, percentThreadsByUser)
+	print 'Pushing metric {} for user {} as system.openfilesperuser.{}'.format(totalThreadsByUser,user,user)
+	statsd.gauge('system.openfilesperuser.{}'.format(user), totalThreadsByUser)
+
+while True:
+
+	for i in userList.splitlines():
+		postMetric(i)
+		time.sleep(10)
