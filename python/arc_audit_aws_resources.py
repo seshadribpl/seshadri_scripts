ed0b72ab3181cc36237a292d192587f1e95e64f5
Seshadrinathan Kothandaraman <kothand@arcesium.com>
2017-10-27 17:56:04 +0530

    automate region retrieval

--- python/arc_audit_aws_resources.py
+++ python/arc_audit_aws_resources.py
@@ -1,42 +1,61 @@
+#!/usr/bin/env python
+
+'''
+Script for identifying instances with missing tags
+
+Author: Seshadri Kothandaraman 27 Oct 2017
+
+'''
+
+
 import boto3
+import subprocess
+
+# First, a teeny snippet to make sure that our credentials are valid for at least 5 minutes
+# If not, run sts init
+#
+
+try:
+    sts_check = 'sts check --window 300'
+    status = subprocess.check_output(sts_check, shell=True, stderr=subprocess.STDOUT)
+
+except subprocess.CalledProcessError:
+    sts_renew = 'sts init'
+    subprocess.check_output(sts_renew, shell=True)
+
+
+# Build up the list of regions
+
+client = boto3.client('ec2')
+regions = [region['RegionName'] for region in client.describe_regions()['Regions']]
+print regions
 
 
 
-# def List_instances_with_tag(tagkey, tagvalue):
-#     ec2client = boto3.client('ec2', region_name='us-east-1')
-#     response = ec2client.describe_instances(Filters=[{'Name': 'tag:'+tagkey,'Values': [tagvalue]}])
-
-#     instancelist = []
-
-#     for reservation in (response["Reservations"]):
-#         for instance in reservation["Instances"]:
-#             instancelist.append(instance["InstanceId"])
-#     return  instancelist
+taglist = ['backup', 'customer']
 
 
 
-def Instance_without_cost_tag(fid):
-    ec2 = boto3.resource('ec2')
-    ec2instance = ec2.Instance(fid)
-    instancename = ''
-    for tags in ec2instance.tags:
-        if tags["Key"] == 'Name':
-            instancename = tags["Value"]
-    print instancename
 
 
-# INSTANCES = List_instances_with_tag('cost', 'baam')
+ec2 = boto3.resource('ec2')
 
 
-# print INSTANCES
+
+def CheckForMissingTag(region,tagname):
+    
+    instances = [i for i in boto3.resource('ec2', region_name=region).instances.all()]
+        
+    for i in instances:
+        if tagname not in [t['Key'] for t in i.tags]:
+            print ' The instance: {0} in the region: {1} is missing the tag: {2}'.format(i.instance_id, region, tagname)
+
+    
 
 
-# for fid in INSTANCES:
-#     print 'Now getting the Name of {}'.format(fid)
-#     Instance_without_cost_tag(fid)
+for region_name in regions:
+    print 'Now checking region {}...'.format(region_name)
+    for tag_name in taglist:
+        CheckForMissingTag(region_name, tag_name)
 
-list1 = ['i-2db8fedd', 'i-1c0e772d' ]
 
-for i in list1:
-    print i
-    Instance_without_cost_tag(i)
